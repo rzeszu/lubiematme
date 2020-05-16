7f7c00e7bbecfcfed23b65f46d33700f6e51b24a
TurboFelipe <dev.turbofelipe@gmail.com>
2020-05-16 15:48:40 +0200

    fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,17 @@ module ApplicationHelper
     def user(value)
         if value == current_user.id
             "card-border"
+        else
+            "card-no-border"
         end
     end
+
+    def user2(value)
+        if value == current_user.id
+            "flex-end"
+        else
+            "flex-start"
+        end
+    end
+
 end
